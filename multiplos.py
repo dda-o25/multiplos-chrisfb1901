eb9e49c466b0eab0d69173922fe5e87785cdd76f
chrisfb1901 <christian.flores@iteso.mx>
2025-09-18 18:49:21 -0600

    multiplos_chrisf

--- multiplos.py
+++ multiplos.py
@@ -1,15 +1,27 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+
+Christian Alonso Flores Burgos
+
+Fecha
+19 de septiembre de 2025
+
+El programa determina si dos valores son múltiplos entre sí.
+
 """
 
 # Declaraciones
-CONSTANTE = valor
-
 # Entradas
-entrada = input()
+numero1 = int(input("Introduce un número:"))
+numero2 = int(input("Introduce otro número:"))
 
 # Proceso
+if numero1<numero2:
+    numero1, numero2 = numero2, numero1
 
+residuo=numero1 % numero2
 
 # Salidas
-print(salida)
+if residuo==0:
+    print("El", numero1, "y", numero2, "sí son múltiplos.")
+else:
+    print("El", numero1, "y", numero2, "no son múltiplos.")
