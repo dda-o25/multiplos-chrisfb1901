cf8ef1a12fd1796463fecd75a37a5dfb5c97bfc5
chrisfb1901 <christian.flores@iteso.mx>
2025-09-18 19:20:43 -0600

    multiplos_chrisf

--- multiplos.py
+++ multiplos.py
@@ -18,10 +18,14 @@ numero2 = int(input("Introduce otro número:"))
 if numero1<numero2:
     numero1, numero2 = numero2, numero1
 
-residuo=numero1 % numero2
+if numero2==0:
+    print("No se puede hacer una división entre 0")
 
-# Salidas
-if residuo==0:
-    print("El", numero1, "y", numero2, "sí son múltiplos.")
 else:
-    print("El", numero1, "y", numero2, "no son múltiplos.")
+    residuo=numero1 % numero2
+    if residuo==0:
+# Salidas
+        print("El", numero1, "es múltiplo de", numero2)
+    else:
+        print("Ninugno de los números es múltiplo de otro.")
+
