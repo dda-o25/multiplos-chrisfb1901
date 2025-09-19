539a55b9bd88274db88afa3debe11c62b2f691cd
chrisfb1901 <christian.flores@iteso.mx>
2025-09-18 19:26:27 -0600

    multiplos_chrisf

--- multiplos.py
+++ multiplos.py
@@ -15,16 +15,14 @@ numero1 = int(input("Introduce un número:"))
 numero2 = int(input("Introduce otro número:"))
 
 # Proceso
-if numero1<numero2:
-    numero1, numero2 = numero2, numero1
 
 if numero2==0:
     print("No se puede hacer una división entre 0")
-
 else:
     residuo=numero1 % numero2
-    if residuo==0:
+
 # Salidas
+    if residuo==0:
         print("El", numero1, "es múltiplo de", numero2)
     else:
         print("Ninugno de los números es múltiplo de otro.")
